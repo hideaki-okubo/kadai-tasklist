15869183c2aeae9897b332765bfb1e83581cfcb5
hideaki.okubo <hidewaki0222@gmail.com>
2019-10-18 09:31:57 +0000

    max10→max191

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -31,7 +31,7 @@ class TasksController extends Controller
     {
         $this->validate($request,[
             "status"=>"required|max:10",
-            "content"=>"required|max:10",
+            "content"=>"required|max:191",
             
         ]);
         $task = new Task;
@@ -60,7 +60,7 @@ class TasksController extends Controller
     {
         $this->validate($request,[
             "status"=>"required|max:10",
-            "content"=>"required|max:10",
+            "content"=>"required|max:191",
         ]);
         $task = Task::find($id);
         $task->status = $request ->status;
