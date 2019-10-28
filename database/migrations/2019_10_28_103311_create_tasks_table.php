c0a11f22b8648f287e4c6202f38eabc775ad1249
hideaki.okubo <hidewaki0222@gmail.com>
2019-10-28 03:47:30 +0000

    users_controller

--- database/migrations/2019_10_28_103311_create_tasks_table.php
+++ database/migrations/2019_10_28_103311_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksTable extends Migration
+class AddUserIdTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::table('tasks', function (Blueprint $table) {
             $table->integer('user_id')->unsigned()->index();
             
             // 外部キー制約
@@ -28,6 +28,6 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('user_id');
     }
 }
